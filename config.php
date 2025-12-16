067dae0ea395ae3f2fd94130926eb3d2bdc338bc
dariaald05 <darianaaldave@gmail.com>
2025-12-16 12:53:13 -0700

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $db_host = "localhost";
 $db_user = "appuser";  // CAMBIA ESTO
 $db_password = "GatitoServer.2025!"; // CAMBIA ESTO
-$db_name = "sistematickets"; // CAMBIA ESTO SI ES DIFERENTE
+$db_name = "tickets_db"; // CAMBIA ESTO SI ES DIFERENTE
 
 // Conexión
 $conexion = new mysqli($db_host, $db_user, $db_password, $db_name);
